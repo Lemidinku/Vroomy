baaf2995483077c3fc447402dbf2f61f1f22f0e5
Lemi Dinku <lemidinq@gmail.com>
2023-12-28 05:11:00 +0300

    build login and signup page

--- src/PublicPages/signup.js
+++ src/PublicPages/signup.js
@@ -1,6 +1,11 @@
 import React  from 'react';
 import { useNavigate } from 'react-router';
 import { supabase } from '../auth';
+import keyImage from '../images/key.png';
+import "../styles/signup.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faUser } from '@fortawesome/free-regular-svg-icons'
+import { faPhone, faLock } from '@fortawesome/free-solid-svg-icons';
 
 function Signup() {
   const navigate = useNavigate()
@@ -92,100 +97,117 @@ function Signup() {
       console.log("Invalid Phone Number")
       return false
     }
-
-
-
-
-
-
-
-
     return true
 
     
   }
   return (
-    <div>
-    <form onSubmit={handleSubmit}>
-      <h1>{userInfo.role}</h1>
-      <div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={userInfo.email}
-            onChange={handleChange}
-            required
-          />
+    <div className='signup__component'>
+      <div className='signup__main'>
+      <div className="block-1__greeting">
+            <h1 className="block-1__head">Welcome!</h1>
+            <p className="block-1__desc">Roam with <span className="block-1__desc--span">Vroomy</span>, Your Reliable car rental companion</p>
         </div>
-        <div>
-          <label htmlFor="fullname">Fullname</label>
+          <form onSubmit={handleSubmit}>
+          <div className='signup__inputs'>
+
+          <div className='signup__input__box'>
+            <FontAwesomeIcon icon={faUser} className='signup__input__icon'/>
           <input
             type="text"
             name="fullname"
             id="fullname"
+            placeholder='Fullname'
             value={userInfo.fullname}
             onChange={handleChange}
             required
           />
         </div>
-        <div>
-          <label htmlFor="username">Username</label>
+        <div className='signup__input__box'>
+            <FontAwesomeIcon icon={faUser} className='signup__input__icon'/>
           <input
             type="text"
             name="username"
             id="username"
+            placeholder='Choose username'
             value={userInfo.username}
             onChange={handleChange}
             required
           />
         </div>
-        <div>
-          <label htmlFor="phone_number">Phone Number</label>
+        <div className='signup__input__box'>
+            <FontAwesomeIcon icon={faEnvelope} className='signup__input__icon'/>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder='Enter Email'
+            value={userInfo.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+
+        <div className='signup__input__box'>
+            <FontAwesomeIcon icon={faPhone} className='signup__input__icon'/>
           <input
             type="tel"
             name="phone_number"
             id="phone_number"
+            placeholder='Enter Phone Number'
             value={userInfo.phone_number}
             onChange={handleChange}
             required
           />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className='signup__input__box'>
+            <FontAwesomeIcon icon={faLock} className='signup__input__icon'/>
           <input
             type="password"
             minLength="6"
             name="password"
             id="password"
+            placeholder='Set password'
             value={userInfo.password}
             onChange={handleChange}
             required
           />
         </div>
-        <div>
-          <label htmlFor="repeatedPassword">Repeat Password</label>
+        <div className='signup__input__box'>
+            <FontAwesomeIcon icon={faLock} className='signup__input__icon'/>
           <input
             type="password"
             minLength="6"
             name="repeatedPassword"
             id="repeatedPassword"
+            placeholder='Confirm password'
             value={userInfo.repeatedPassword}
             onChange={handleChange}
             required
           />
         </div>
       </div>
+      <div className="signup__type">
+        <div>
       <label htmlFor="owner">Owner</label>
       <input type='radio' name="role"  id="owner" value="owner" checked={userInfo.role == "owner"} onChange={changeRole} required></input>
+      </div>
+      <div>
       <input type='radio' name="role"  id="renter" value="renter" checked={userInfo.role == "renter"} onChange={changeRole} required></input>
       <label htmlFor="renter">Renter</label>
-      <button >Signup</button>
-      </form>
+      </div>
+      </div>
+      <button className='signup__button'>Signup</button>
+          </form>
+        <p className='signup__account'>Already have an account? <a href='#'> login</a></p>
+
+      </div>
+      <div className='signup__graphic'>
+        <img className='signup__img' src={keyImage}/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Signup;
